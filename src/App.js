330701fbb9c431ba031042e96b99b08031cd1e07
HeideQueen <lucyfabz@gmail.com>
2020-01-03 18:50:01 -0600

    finish pomodoro timer functionality

--- src/App.js
+++ src/App.js
@@ -13,14 +13,27 @@ const App = () => {
   const [sessionLength, setSessionLength] = useState(25);
   const [breakLength, setBreakLength] = useState(5);
   const [isRunning, setIsRunning] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+  const [onBreak, setOnBreak] = useState(false);
   const [timeLeft, setTimeLeft] = useState('');
+  const [pausedTime, setPausedTime] = useState(0);
 
   useEffect(() => {
     let countdown;
 
     if (isRunning) {
       const timer = sessionLength => {
-        const seconds = sessionLength * 60;
+        let seconds;
+        if (isPaused) {
+          seconds = pausedTime;
+          setIsPaused(false);
+        } else {
+          if (onBreak) {
+            seconds = breakLength * 60;
+          } else {
+            seconds = sessionLength * 60;
+          }
+        }
         const now = Date.now();
         const then = now + seconds * 1000;
         displayTimeLeft(seconds);
@@ -29,8 +42,10 @@ const App = () => {
           const secondsLeft = Math.round((then - Date.now()) / 1000);
           if (secondsLeft < 0) {
             clearInterval(countdown);
+            setOnBreak(!onBreak);
             return;
           }
+          setPausedTime(secondsLeft);
           displayTimeLeft(secondsLeft);
         }, 1000);
       };
@@ -46,10 +61,11 @@ const App = () => {
       }
 
       timer(sessionLength);
+      console.log(onBreak);
     }
 
     return () => clearInterval(countdown);
-  }, [isRunning, sessionLength]);
+  }, [isRunning, onBreak]);
 
   const handleSettingsClick = type => {
     switch (type) {
@@ -76,12 +92,15 @@ const App = () => {
 
   const handleReset = () => {
     setIsRunning(false);
+    setIsPaused(false);
+    setOnBreak(false);
     setTimeLeft('');
     setSessionLength(25);
     setBreakLength(5);
   };
 
   const handlePlayPause = () => {
+    if (isRunning) setIsPaused(true);
     setIsRunning(!isRunning);
   };
 
@@ -103,7 +122,11 @@ const App = () => {
         handleSettingsClick={handleSettingsClick}
       />
 
-      <TimerDisplay sessionLength={sessionLength} timeLeft={timeLeft} />
+      <TimerDisplay
+        sessionLength={sessionLength}
+        timeLeft={timeLeft}
+        onBreak={onBreak}
+      />
 
       <TimerControls
         handleReset={handleReset}
--- src/components/timer-display/timer-display.component.jsx
+++ src/components/timer-display/timer-display.component.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
-const TimerDisplay = ({ sessionLength, timeLeft }) => {
+const TimerDisplay = ({ sessionLength, timeLeft, onBreak }) => {
   return (
     <Paper>
-      <Typography id='timer-label'>Session</Typography>
+      <Typography id='timer-label'>{onBreak ? 'Break' : 'Session'}</Typography>
       <Typography id='time-left'>
         {timeLeft
           ? timeLeft
